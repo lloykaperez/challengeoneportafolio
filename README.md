ef51b3cf2e682587ef9b3c8c96d4a58a09161fb3
Lloyka Pérez <105314188+lloykaperez@users.noreply.github.com>
2022-10-23 16:35:24 -0300

    Terminada parte html y css

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # challengeoneportafolio
 Challenge ONE Front-End Sprint 01: del Figma a HTML y CSS
-
-
-
---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector('.menu');
+
+    window.addEventListener('scroll', function() {
+        manu.classList.toggle('active', window.scrolly >0)
+    })
